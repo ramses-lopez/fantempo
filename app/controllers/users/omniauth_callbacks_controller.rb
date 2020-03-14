236a6f24254c805cad2e6a202555257f552634d0
Ramses <ramses.lopezs@gmail.com>
2020-03-14 00:17:57 -0400

    rubocop

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,37 @@
+# frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+      if is_navigational_format?
+        set_flash_message(:notice, :success, kind: 'Facebook')
+      end
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      session['devise.facebook_data'] = request.env['omniauth.auth']
+      sign_in_and_redirect @user, event: :authentication
     end
   end
 
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except("extra")
-      set_flash_message(:notice, :success, :kind => "GoogleOAuth2") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+      session['devise.google_oauth2_data'] = request.env['omniauth.auth'].except('extra')
+      if is_navigational_format?
+        set_flash_message(:notice, :success, kind: 'GoogleOAuth2')
+      end
     else
-      sign_in_and_redirect @user, :event => :authentication
-      session["devise.google_oauth2_data"] = request.env["omniauth.auth"].except("extra")
+      session['devise.google_oauth2_data'] = request.env['omniauth.auth'].except('extra')
+      byebug
+      sign_in_and_redirect @user, event: :authentication
     end
   end
 
   def failure
+    # todo
     redirect_to root_path
   end
+
 end
