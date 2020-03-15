d4bafc8fb8238490003ec04711d633bc81a8d537
Ramses <ramses.lopezs@gmail.com>
2020-03-15 14:49:00 -0400

    Add bootstrap dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,11 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
 
+import "./styles.scss";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
