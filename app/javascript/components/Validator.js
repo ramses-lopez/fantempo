1e9decee6cb1e366f9cc83803a5a0224226ebcb0
Ramses <ramses.lopezs@gmail.com>
2020-03-15 13:39:23 -0400

    Functional validator component

--- app/javascript/components/Validator.js
+++ app/javascript/components/Validator.js
@@ -1,15 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ErrorBoundary from "./ErrorBoundary"
-import ValidatorInput from "./ValidatorInput"
 
 class Validator extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       showValidator: true,
-      code: null
+      input0: React.createRef(),
+      input1: React.createRef(),
+      input2: React.createRef()
     }
+
     this.sendCode = this.sendCode.bind(this)
     this.resendCode = this.resendCode.bind(this)
     this.handlePaste = this.handlePaste.bind(this)
@@ -25,16 +27,51 @@ class Validator extends React.Component {
 
   handlePaste(event) {
     event.clipboardData.items[0].getAsString(text => {
-
-      const code = text.split("").map((char, index) => {
-        return Number.isInteger(Number(char)) ? Number(char) : ''
+      text.split("").forEach((char, i) => {
+        const num = Number(char)
+        if (!Number.isInteger(num)) return
+        this.state[`input${i}`].current.value = this.state[`input${i}`] ? num : ""
       })
-
-      this.setState({ code: code })
     })
   }
 
+  handleFocus(event, nextNode, prevNode){
+    const key = String.fromCharCode(event.which).toLowerCase()
+
+    if ([9].includes(event.keyCode) || (event.metaKey || event.ctrlKey) && key === 'v') return
+
+    event.preventDefault()
+
+    if (!Number.isInteger(Number(key))) return
+
+    //go back if backspace or delete
+    if ([8, 46].includes(event.keyCode)) {
+      event.target.value = ''
+      if(prevNode !== null) prevNode.current.focus()
+      return
+    }
+
+    if(Number.isInteger(Number(key))){
+      event.target.value = key
+      if (nextNode !== null) nextNode.current.focus()
+    }
+  }
+
   render() {
+    const FancyInput = React.forwardRef((props, ref) => {
+      return (
+        <input
+          index={props.index}
+          ref={ref}
+          className="child"
+          onChange={props.onchangeHandler}
+          onKeyDown={e => props.keyPressHandler(e, props.nextRef, props.prevRef)}
+          maxLength="1"
+          value={props.value}
+        />
+      )
+    })
+
     const phoneInput = (
       <>
         <select>
@@ -47,27 +84,40 @@ class Validator extends React.Component {
       </>
     )
 
-    const code = this.state.code
+    const input0 = this.state.input0
+    const input1 = this.state.input1
+    const input2 = this.state.input2
 
     const codeInput = (
-      <React.Fragment>
-        <div>
-          <div className="container" onPaste={this.handlePaste}>
-            <ValidatorInput value={code && code[0]} index={0} />
-            <ValidatorInput value={code && code[1]} index={1} />
-            <ValidatorInput value={code && code[2]} index={2} />
-            <ValidatorInput value={code && code[3]} index={3} />
-          </div>
-        </div>
-
-        <a href="#" onClick={this.resendCode}>
-          Reenviar código
-        </a>
-      </React.Fragment>
-    );
+      <div className="parent" onPaste={this.handlePaste}>
+        <FancyInput
+          onPaste={this.handlePaste}
+          ref={input0}
+          prevRef={null}
+          nextRef={input1}
+          keyPressHandler={this.handleFocus}
+        ></FancyInput>
+        <FancyInput
+          onPaste={this.handlePaste}
+          ref={input1}
+          prevRef={input0}
+          nextRef={input2}
+          keyPressHandler={this.handleFocus}
+        ></FancyInput>
+        <FancyInput
+          onPaste={this.handlePaste}
+          ref={input2}
+          prevRef={input1}
+          nextRef={null}
+          keyPressHandler={this.handleFocus}
+        ></FancyInput>
+      </div>
+    )
 
     return (
       <ErrorBoundary>
+        <div>876</div>
+        <div>ABC</div>
         {this.state.showValidator ? codeInput : phoneInput}
       </ErrorBoundary>
     )
