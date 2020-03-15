af784c74c31c6ef12ae5f41767892f8036b40e11
Ramses <ramses.lopezs@gmail.com>
2020-03-15 17:03:56 -0400

    - Add fonts - Add trigger reload for views

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+
+// trigger browser reloads
+const chokidar = require('chokidar')
+environment.config.devServer.before = (app, server) => {
+  chokidar.watch([
+    `${__dirname}/../stylesheets/`,
+    `${__dirname}/../../app/views/`
+  ])
+    .on('change', () => {
+      server.sockWrite(server.sockets, 'content-changed')
+    })
+}
+
 module.exports = environment.toWebpackConfig()
