73de57eac17e97506f0910875094361a419b759f
Ramses <ramses.lopezs@gmail.com>
2020-03-22 11:02:53 -0400

    Add mapbox & fix positioning

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,11 +1,65 @@
 import React from "react"
 import PropTypes from "prop-types"
+import mapboxgl from "mapbox-gl";
+
 class Map extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 5
+    };
+  }
+
+  componentDidMount() {
+    mapboxgl.accessToken = this.props.mapboxToken
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    })
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      })
+
+      console.log(this.state.lng, this.state.lat)
+    })
+  }
+
+  handleSaveLocation(){
+    console.log("handleSaveLocation");
+  }
+
+  render() {
+    const countryList = [
+      <option key="0">Toronto, Canada</option>,
+      <option key="1">Miami, USA</option>,
+      <option key="2">Madrid, Spain</option>
+    ]
+
+    const locationName = "Toronto y alrededores (10 km)"
+
     return (
-      <React.Fragment>
-        Mapbox Token: {this.props.mapboxToken}
-      </React.Fragment>
+      <>
+        <select className="custom-select mb-2" value={1} onChange={() => {}}>
+          {countryList}
+        </select>
+        <div
+          ref={el => (this.mapContainer = el)}
+          className="mapContainer mb-2"
+        />
+        <div className="text-left subtitle mb-5">{locationName}</div>
+        <button className="btn btn-primary" onClick={ this.handleSaveLocation }>
+          Aceptar
+        </button>
+      </>
     );
   }
 }
@@ -13,4 +67,5 @@ class Map extends React.Component {
 Map.propTypes = {
   mapboxToken: PropTypes.string
 };
+
 export default Map
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+//
+// https://github.com/mapbox/mapbox-gl-js/issues/3422
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null) ? [] : [nodeModulesLoader.exclude]
+}
+nodeModulesLoader.exclude.push(/mapbox-gl/)
+
+console.log('-----------------------')
+console.log(environment.loaders);
+console.log('-----------------------')
+
 module.exports = environment
