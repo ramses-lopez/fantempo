9be140a7a8e23d578d6959d3382a2d6b1e5248a0
Ramses <ramses.lopezs@gmail.com>
2020-03-23 18:29:48 -0400

    UI Adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def redirect_if_logged_out
     redirect_to login_path if current_user.nil?
   end
+
+  # UI Helpers
+  def responsive_col_css
+    "col-xl-6 col-lg-6 col-md-6 col-sm-10"
+  end
+
 end
