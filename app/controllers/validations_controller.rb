211dd1650898dec08143c11a223948a8290f1097
Ramses <ramses.lopezs@gmail.com>
2020-03-22 21:59:08 -0400

    Enable twilio for prod

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -6,19 +6,25 @@ class ValidationsController < ApplicationController
   # send validation code
   def create
     # TODO: Uncomment to enable twilio
-    # response = Validation.send_verification(validation_params[:phone_number])
-    response = { message: 'ok' }
+    unless Rails.env.development?
+      response = Validation.send_verification(validation_params[:phone_number])
+    else
+      response = { message: 'ok' }
+    end
     render json: response.to_json
   end
 
   # validate code
   def update
     # TODO: Uncomment to enable twilio
-    # response = Validation.validate_code(
-    #             validation_params[:phone_number],
-    #             validation_params[:validation_code]
-    #           )
-    response = { message: 'ok' }
+    unless Rails.env.development?
+      response = Validation.validate_code(
+                  validation_params[:phone_number],
+                  validation_params[:validation_code]
+                )
+    else
+      response = { message: 'ok' }
+    end
     render json: response.to_json
   end
 
