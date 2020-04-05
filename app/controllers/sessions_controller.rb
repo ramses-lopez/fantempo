0064581bf0d9a4c3ffed43aa35d06914707e3709
Ramses <ramses.lopezs@gmail.com>
2020-04-05 00:04:31 -0400

    Redirect if logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  before_action :redirect_if_logged_in
+
   def index
     @artist_name = 'Tu artista favorito'
-    if params[:artist].present?
-      artist = Artist.find_by(id: params[:artist])
-      if artist.present?
-        session[:artist_id] = params[:artist]
-        @artist_name = artist.name
-      end
+
+    return unless params[:artist].present?
+
+    artist = Artist.find_by(id: params[:artist])
+    if artist.present?
+      session[:artist_id] = params[:artist]
+      @artist_name = artist.name
     end
   end
 end
